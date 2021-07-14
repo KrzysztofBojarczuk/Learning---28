c17be238b6131dd670a3b62c0aa219a1acc9d931
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-07-14 08:41:46 +0200

    Add project files.

---
+++ Leraning---149/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Leraning___149
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zyski ze sprzedazy płytek drukownaych");
+            while (true)
+            {
+                try
+                {
+                    Calc();
+                    Console.WriteLine("Jesli chcesz zakończyć program wpisz Tak / tak. Jeśli chcesz dalej kontynować wciśjnij inny przycisk:");
+                    string end = Console.ReadLine();
+                    if (end == "Tak" || end == "tak" || end == "tAk" || end == "tAk")
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void Calc()
+        {
+            Console.WriteLine("Wprowadź cenne detalicznych płytek:");
+            double price = double.Parse(Console.ReadLine());
+
+            double profits = price * 0.4;
+            Console.WriteLine($"Zysk ze sprzedazy wynoi {profits.ToString("c")}");
+            StreamWriter inputDate = File.CreateText("Zysk.text");
+            inputDate.WriteLine($"Zysk ze sprzedazy wynoi {profits.ToString("c")}");
+            inputDate.Close();
+        }
+    }
+}
